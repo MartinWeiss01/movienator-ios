071d234a91d58ddaf94bebae9d32fb78375b8ad0
Martin Weiss <xweiss3@mendelu.cz>
2023-04-27 15:21:35 +0200

    Init project

---
+++ Movienator/Movienator/MovienatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  MovienatorApp.swift
+//  Movienator
+//
+//  Created by Martin Weiss on 27.04.2023.
+//
+
+import SwiftUI
+
+@main
+struct MovienatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
