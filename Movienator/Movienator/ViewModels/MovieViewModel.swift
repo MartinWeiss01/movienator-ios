93d437735ef66c4706db37a6fb74accae47e12a6
Martin Weiss <xweiss3@mendelu.cz>
2023-05-10 13:02:00 +0200

    ci(dev): add temp files

--- Movienator/Movienator/ViewModels/MovieViewModel.swift
+++ Movienator/Movienator/ViewModels/MovieViewModel.swift
@@ -22,5 +22,40 @@ class MovieViewModel: ObservableObject {
         self.moc = moc
     }
     
+    func convertSearchResponseToMovieItem(response: SearchMovieResponseDTO) -> [MovieItem]? {
+        let coordinatesSplit = response.results.
+
+        let latitude = Double(coordinatesSplit.first ?? "")
+        let longitude = Double(coordinatesSplit.last ?? "")
+
+        guard
+            let latitude,
+            let longitude
+        else {
+            return nil
+        }
+        
+        return MapItem(
+            name: "IP \(response.region)",
+            style: .Modern,
+            imageAssetName: UIImage(),
+            coordinates: CLLocationCoordinate2D(
+                latitude: latitude,
+                longitude: longitude
+            ),
+            locationType: .House
+        )
+    }
     
+    func searchMovies(title: String) async throws {
+        isSearching = true
+        
+        defer {
+            isSearching = false
+        }
+        
+        let searchMovieResponseDTO: SearchMovieResponseDTO = try await apiManager.request(SearchRouter.movie(title: title))
+        
+        guard let resultItems = con
+    }
 }
