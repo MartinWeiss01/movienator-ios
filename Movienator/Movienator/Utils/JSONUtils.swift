f255b123d484a4152bfe5a94d83011f07e28766d
Martin Weiss <xweiss3@mendelu.cz>
2023-06-21 10:52:04 +0200

    ci(utils): add JSON encode & decode function
    Since list of genres will be stored in Core Data as array of strings (name of genres), VM will encode array to string and on fetching items it will decode this string to [String]

--- Movienator/Movienator/Utils/JSONUtils.swift
+++ Movienator/Movienator/Utils/JSONUtils.swift
@@ -7,6 +7,11 @@
 
 import Foundation
 
+enum JSONError: Error {
+    case encodeFail
+    case decodeFail
+}
+
 class JSONUtils {
     static let decoder: JSONDecoder = {
         let decoder = JSONDecoder()
@@ -14,4 +19,26 @@ class JSONUtils {
         decoder.dateDecodingStrategy = .formatted(DateUtils.format)
         return decoder
     }()
+    
+    static let encoder: JSONEncoder = {
+        let encoder = JSONEncoder()
+        encoder.keyEncodingStrategy = .convertToSnakeCase
+        return encoder
+    }()
+    
+    static func encodeStringArray(_ array: [String]) throws -> String {
+        let data = try encoder.encode(array)
+        guard let jsonString = String(data: data, encoding: .utf8) else {
+            throw JSONError.encodeFail
+        }
+        return jsonString
+    }
+    
+    static func decodeStringArray(from jsonString: String) throws -> [String] {
+        guard let data = jsonString.data(using: .utf8) else {
+            throw JSONError.decodeFail
+        }
+        let array = try decoder.decode([String].self, from: data)
+        return array
+    }
 }
