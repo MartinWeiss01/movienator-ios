cd3f701afd5a5f260ae97432ed55767988ce5d13
Martin Weiss <xweiss3@mendelu.cz>
2023-06-21 10:50:09 +0200

    ci(utils): add GenreUtils
    Add functions to convert genre ids to strings

---
+++ Movienator/Movienator/Utils/GenreUtils.swift
@@ -0,0 +1,32 @@
+//
+//  GenreUtils.swift
+//  Movienator
+//
+//  Created by Martin Weiss on 21.06.2023.
+//
+
+import Foundation
+
+class GenreUtils {
+    static func getGenreNames(genreIds: [Int64]?) -> [String] {
+        if let genresList = genreIds {
+            let genreNames = genresList.compactMap { id in
+                GenreList(rawValue: Int(id))?.name
+            }
+            return genreNames
+        } else {
+            return []
+        }
+    }
+    
+    static func getGenreString(genreIds: [Int64]?) -> String {
+        let genreName = getGenreNames(genreIds: genreIds)
+        let genreConcat = genreName.joined(separator: ", ")
+        return genreConcat
+    }
+    
+    static func getGenreString(genres: [String]) -> String {
+        let genreConcat = genres.joined(separator: ", ")
+        return genreConcat
+    }
+}
